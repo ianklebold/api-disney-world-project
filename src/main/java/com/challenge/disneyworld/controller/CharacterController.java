ebfa8ce3be86cdda2530c224dc9b84c26f8a21df
ianklebold <ianstgo@gmail.com>
2022-01-14 17:12:49 -0300

    Character Controller and Appearance Controller is ready

---
+++ src/main/java/com/challenge/disneyworld/controller/CharacterController.java
@@ -0,0 +1,10 @@
+package com.challenge.disneyworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/disneyworld/api/v1/characters")
+public class CharacterController {
+    
+}
