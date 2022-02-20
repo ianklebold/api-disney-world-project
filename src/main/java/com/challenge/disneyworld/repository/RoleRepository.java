40026105e8ba52232b08943eab66bc19f18c2b51
ianklebold <ianstgo@gmail.com>
2022-02-20 20:26:47 -0300

    003 - REFACTOR - Tiny Refactor

---
+++ src/main/java/com/challenge/disneyworld/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.challenge.disneyworld.repository;
+
+import com.challenge.disneyworld.entity.Role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
